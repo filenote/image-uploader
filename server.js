9fb704b8fa418227fd195f7e59f0641df8d08741
Pedro Rodriguez <prodriguez.eh@gmail.com>
2019-11-26 03:14:04 -0600

    fully running in aws

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 
